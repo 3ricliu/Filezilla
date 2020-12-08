6cfbbb0a3a73d2f466f600dce65a974a1b0dfe18
Eric Liu <ericliu4943@gmail.com>
2020-12-07 21:00:41 -0800

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Eric Liu on 12/7/20.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
