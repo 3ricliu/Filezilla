e5002442b00038598494b4a1f83faf61610c059b
Eric Liu <ericliu4943@gmail.com>
2020-12-11 18:46:01 -0800

    Edit view + loading and saving to user defaults

--- Flashzilla/CardView.swift
+++ Flashzilla/CardView.swift
@@ -12,9 +12,11 @@ struct CardView: View {
     var removal: (() -> Void)? = nil
     
     @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
+    @Environment(\.accessibilityEnabled) var accessibilityEnabled
 
     @State private var isShowingAnswer = false
     @State private var offset = CGSize.zero
+    @State private var feedback = UINotificationFeedbackGenerator()
 
     var body: some View {
         ZStack {
@@ -24,6 +26,7 @@ struct CardView: View {
                         ? Color.white
                         : Color.white
                             .opacity(1 - Double(abs(offset.width / 50)))
+
                 )
                 .background(
                     differentiateWithoutColor
@@ -34,14 +37,20 @@ struct CardView: View {
                 .shadow(radius: 10)
             
             VStack {
-                if isShowingAnswer {
-                    Text(card.answer)
-                        .font(.title)
-                        .foregroundColor(.blue)
-                } else {
-                    Text(card.prompt)
+                if accessibilityEnabled {
+                    Text(isShowingAnswer ? card.answer : card.prompt)
                         .font(.largeTitle)
                         .foregroundColor(.black)
+                } else {
+                    if isShowingAnswer {
+                        Text(card.answer)
+                            .font(.title)
+                            .foregroundColor(.blue)
+                    } else {
+                        Text(card.prompt)
+                            .font(.largeTitle)
+                            .foregroundColor(.black)
+                    }
                 }
             }
             .padding(20)
@@ -51,13 +60,21 @@ struct CardView: View {
         .rotationEffect(.degrees(Double(offset.width / 5)))
         .offset(x: offset.width  * 5, y: 0)
         .opacity(2 - Double(abs(offset.width / 50)))
+        .accessibility(addTraits: .isButton)
         .gesture(
             DragGesture()
                 .onChanged { gesture in
                     self.offset = gesture.translation
+                    self.feedback.prepare()
                 }
                 .onEnded { _ in
                     if abs(self.offset.width) > 100 {
+                        if self.offset.width > 0 {
+                            self.feedback.notificationOccurred(.success)
+                        } else {
+                            self.feedback.notificationOccurred(.error)
+                        }
+                        
                         self.removal?()
                     } else {
                         self.offset = .zero
@@ -67,6 +84,7 @@ struct CardView: View {
         .onTapGesture {
             self.isShowingAnswer.toggle()
         }
+        .animation(.spring())
     }
 }
 
